71c1292b532044dd14eec0c4725c97ff29630cb0
humlium <humlium@gmail.com>
2015-10-15 23:12:31 +0700

    first commit

--- src/main/java/web/Station.java
+++ src/main/java/web/Station.java
@@ -12,10 +12,41 @@ public class Station {
 	public Double latitude;
 	public Double longitude;
 
+        public Long getId(){
+            return id;
+        }
+        public String getState(){
+            return state;
+        }        
 	public String getSuburb() {
 		return suburb;
 	}
-	public Long getId() {
-		return id;
-	}
+        public String getAddress(){
+            return address;
+        }
+        public Double getLatitude(){
+            return latitude;
+        }
+        public Double getLongitude(){
+            return longitude;
+        }
+        
+        public void setId(Long tmp){
+            id = tmp;
+        }
+        public void setState(String tmp){
+            state = tmp;
+        }
+        public void setSuburb(String tmp){
+            suburb = tmp;
+        }
+        public void setAddress(String tmp){
+            address = tmp;
+        }
+        public void setLatitude(Double tmp){
+            latitude = tmp;
+        }
+        public void setLongitude(Double tmp){
+            longitude = tmp;
+        }
 }
--- src/main/java/web/Web.java
+++ src/main/java/web/Web.java
@@ -38,8 +38,13 @@ public class Web {
 		Double top, Double bottom,
 		Double left, Double right) {
 		Session database = factory.openSession();
-		Query query = database.createQuery("from Station");
-		List result = query.list();
+		Query query = database.createQuery("from Station where latitude > :bottom"
+                        + " and latitude < :top and longitude > :left and longitude < :right");
+		query.setParameter("bottom", bottom);
+                query.setParameter("top", top);
+                query.setParameter("left", left);
+                query.setParameter("right", right);
+                List result = query.list();
 		database.close();
 		return result;
 	}
@@ -108,4 +113,39 @@ public class Web {
 		database.close();
 		return result;
 	}
+        @RequestMapping("/suburb") @ResponseBody
+        List sSuburb(String name){
+            Session db = factory.openSession();
+            Query q = db.createQuery(
+                    "from Station where Suburb like :x");
+            q.setParameter("x", "%" + name + "%");
+            List result = q.list();
+            db.close();
+            return result;
+        }
+        
+        @RequestMapping("/postcode/{code}") @ResponseBody
+        List sCode(@PathVariable String code){
+            Session db = factory.openSession();
+            Query q = db.createQuery(
+                    "from Station where postcode like :x");
+            q.setParameter("x", code);
+            List list = q.list();
+            
+            db.close();
+            return list;
+        }
+        
+        @RequestMapping("/searchPS/{code}") @ResponseBody
+        List searchPS(@PathVariable String code){
+            Session db = factory.openSession();
+            Query q = db.createQuery(
+                    "from Station where Suburb like :x or address like :x");
+            q.setParameter("x", "%" + code + "%");
+            List list = q.list();
+            db.close();
+            return list;
+        }
+        
+        
 }
